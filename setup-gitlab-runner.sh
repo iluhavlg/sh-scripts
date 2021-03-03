5e1e649c03331ab5f2c50d5016315e065c063152
iluhavlg <iluha_t@bk.ru>
2021-03-03 16:54:49 +0300

    Create setup-gitlab-runner.sh

---
+++ setup-gitlab-runner.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+# For Debian/Ubuntu/Mint add repository
+curl -L https://packages.gitlab.com/install/repositories/runner/gitlab-runner/script.deb.sh | sudo bash
+# Install
+export GITLAB_RUNNER_DISABLE_SKEL=true; sudo -E apt-get install gitlab-runner
+sudo gitlab-runner restart
+echo "Файл сервиса runner (сделать запуск от root)"
+echo "sudo nano /etc/systemd/system/gitlab-runner.service"
+echo "sudo systemctl daemon-reload"
+echo "sudo gitlab-runner restart"
