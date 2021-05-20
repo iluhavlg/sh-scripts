97d005247bdab71aefe0a3b2e93d8ffd5b9f04b2
iluhavlg <iluha_t@bk.ru>
2021-05-20 14:20:08 +0300

    Update prepare-server-for-k8s.sh

--- prepare-server-for-k8s.sh
+++ prepare-server-for-k8s.sh
@@ -10,7 +10,7 @@ sudo apt -y install curl apt-transport-https
 curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
 echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
 sudo apt update 
-sudo apt -y install vim git curl wget
+sudo apt -y install vim git curl wget htop
 sudo apt-get install -qy kubelet=$K8S_VERSION kubectl=$K8S_VERSION kubeadm=$K8S_VERSION
 sudo apt-mark hold kubelet kubeadm kubectl
 kubectl version --client && kubeadm version
